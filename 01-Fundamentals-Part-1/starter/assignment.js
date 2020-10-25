9dd13220d9f34e41d87a6f6e301bd335edaeef70
capenapple <capen.apple@gmail.com>
2020-10-25 18:02:55 -0500

    Work
    IT

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,51 @@
+/*
+const country = "United States"
+const continent = "North America"
+let population = 331
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+let isIsland = true
+let language;
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
+
+language = 'English'
+// country = 'Mexico'
+
+
+console.log(population / 2);
+console.log(population ++);
+console.log(population > 6);
+console.log(population < 33);
+
+console.log(country + ' is on '+ continent + ' with a population of ' + population + ' that speak ' + language)
+*/
+
+let markMass = 78
+let markHeight = 1.69
+let johnMass = 92
+let johnHeight = 1.95
+let johnBMI = johnMass / johnHeight ** 2
+let markBMI = markMass / markHeight ** 2
+
+console.log(johnBMI, markBMI);
+let markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
+markMass = 95
+markHeight = 1.88
+johnMass = 85
+johnHeight = 1.76
+johnBMI = johnMass / johnHeight ** 2
+markBMI = markMass / markHeight ** 2
+
+console.log(johnBMI, markBMI);
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,75 @@
+/*let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Capen");
+console.log(23);
+
+
+let firstName = 'Capen';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Hello');
+
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(typeof year);
+console.log(year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1978
+// birthYear = 1990
+
+// const job;
+
+var job = 'programmer'
+job = 'teacher'
+
+
+const now = 2037;
+const ageCapen = now - 1978;
+const ageEvelyn = now - 1981;
+console.log(ageCapen, ageEvelyn);
+
+console.log(ageCapen * 2, ageEvelyn /10, 2 ** 3);
+
+const firstName = 'Capen';
+const lastName = 'Apple';
+console.log(firstName + ' ' + lastName);
+
+//Assignment operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+//Comparision operators
+console.log(ageCapen > ageEvelyn);
+console.log(ageEvelyn >= 18);
+
+const isFullAge = ageEvelyn >= 20
+
+console.log(now - 1991 > now - 2018);
+*/
+
