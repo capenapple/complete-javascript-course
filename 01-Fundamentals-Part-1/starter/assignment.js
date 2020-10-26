d9cc9eb0df16f610ec35e5ecbc8849048c98784e
Capen Apple <capen.apple@gmail.com>
2020-10-26 02:06:07 -0500

    Part 1
    Part 1

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -3,31 +3,32 @@ const country = "United States"
 const continent = "North America"
 let population = 331
 
-console.log(country)
-console.log(continent)
-console.log(population)
+// console.log(country)
+// console.log(continent)
+// console.log(population)
 
 let isIsland = true
 let language;
 
-console.log(typeof country);
-console.log(typeof continent);
-console.log(typeof population);
-console.log(typeof isIsland);
-console.log(typeof language);
+// console.log(typeof country);
+// console.log(typeof continent);
+// console.log(typeof population);
+// console.log(typeof isIsland);
+// console.log(typeof language);
 
 language = 'English'
 // country = 'Mexico'
 
 
-console.log(population / 2);
-console.log(population ++);
-console.log(population > 6);
-console.log(population < 33);
+// console.log(population / 2);
+// console.log(population ++);
+// console.log(population > 6);
+// console.log(population < 33);
 
-console.log(country + ' is on '+ continent + ' with a population of ' + population + ' that speak ' + language)
+// console.log(country + ' is on '+ continent + ' with a population of ' + population + ' that speak ' + language)
 */
-
+/*
+// Coding Challenge #1
 let markMass = 78
 let markHeight = 1.69
 let johnMass = 92
@@ -49,3 +50,24 @@ markBMI = markMass / markHeight ** 2
 console.log(johnBMI, markBMI);
 markHigherBMI = markBMI > johnBMI;
 console.log(markHigherBMI);
+*/
+/*
+let description = `${country} is in ${continent}, and it's ${population} million people speak ${language}`
+console.log(description)
+
+let worldAverage = 33
+
+if (population > worldAverage) {
+    console.log(`${country} has ${population - worldAverage} more people than the world average!`)
+} else {
+  console.log(`${country} has ${worldAverage - population} less people than the world average!`)
+}
+*/
+/*
+//coding challenge #2
+if (johnBMI > markBMI) {
+    console.log(`John's BMI of ${johnBMI} is higher than Mark's BMI of ${markBMI}!`);
+} else {
+    console.log(`Mark's BMI of ${markBMI} is higher than John's BMI of ${johnBMI}!`);
+}
+*/
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -71,5 +71,92 @@ console.log(ageEvelyn >= 18);
 const isFullAge = ageEvelyn >= 20
 
 console.log(now - 1991 > now - 2018);
+
+
+const firstName = 'Capen';
+const job = 'engineer';
+const birthYear = 1978;
+const currentYear = '2020';
+
+const capen = "I'm " + firstName + ', a ' + (currentYear - birthYear) + ' years old ' + job + '!';
+console.log(capen);
+
+const capenNew = `I'm ${firstName}, a ${currentYear - birthYear} years old ${job}!`;
+console.log(capenNew);
+
+console.log('String with \n\
+multiple\n\
+lines');
+
+console.log(`New string
+with 
+lines`);
+*/
+/*
+const age = 14;
+
+if (age >= 18) {
+console.log(`You can start driving test`);
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`You need to wait ${yearsLeft} to drive`);
+}
+
+let century
+const birthYear = 1978;
+if (birthYear >= 2000) {
+    century = 21;
+} else {
+  century = 20;
+}
+
+console.log(century)
+*/
+/*
+//type conversion
+const inputYear = '1991';
+console.log(Number(inputYear) + 18)
+console.log(inputYear + 18);
+
+console.log(Number('Capen'))
+console.log(typeof Nan);
+console.log(String(31), 31);
+
+//type coercion
+console.log('I am ' + 23 + ' years old!')
+console.log('23' - '10' - 3)
+console.log('23' * '2')
+console.log('23' > '18')
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+
+console.log('9' - '5'); //4
+console.log('19' - '13' + '17'); //617
+console.log('19' - '13' + 17) ; //23
+console.log('123' < 57); //false
+console.log(5 + 6 + '4' + 9 - 4 - 2) //1143
 */
 
+// 5 falsy value 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Capen'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 10;
+if (money) {
+    console.log("Don't spend it all");
+} else {
+    console.log("You should get a job");
+}
+
+let height = 0;
+
+if (height) {
+    console.log('height is defined');
+} else {
+    console.log('height is undefined');
+}
